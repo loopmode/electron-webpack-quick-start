79f2b8d5ccdb8cb42129e1d52c2b4a200dd998c9
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-04-12 09:50:53 +0200

    disable electron security warnings in development

--- src/main/index.js
+++ src/main/index.js
@@ -6,6 +6,9 @@ import { format as formatUrl } from 'url'
 import initDevTools from './initDevTools'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
+if (isDevelopment) {
+  process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+}
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow
