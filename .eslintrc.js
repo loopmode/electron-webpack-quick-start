db6f0815cf5d5c632fc4dd6ca6d52afe27c20a62
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-04-12 08:13:28 +0200

    allow console and debug statements in development

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,10 @@ module.exports = {
     react: {
       version: 'detect'
     }
+  },
+  rules: {
+    // allow console and debugger in development
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
