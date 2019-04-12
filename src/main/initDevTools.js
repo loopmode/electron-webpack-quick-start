f740a5a75a3654b7317426524a044040935591fc
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-04-12 11:53:48 +0200

    manually install electron-react-devtools

--- src/main/initDevTools.js
+++ src/main/initDevTools.js
@@ -1,26 +1,18 @@
 import { BrowserWindow } from 'electron'
 import * as path from 'path'
 
-// custom extensions to install
-// use BrowserWindow.getDevToolsExtensions() to get name
-
-const extensions = [
+const customExtensions = [
   {
     name: 'Immutable.js Object Formatter',
     dir: path.join(__static, 'extensions/immutablejs-object-formatter/1.9.2_0')
+  },
+  {
+    name: 'React Developer Tools',
+    dir: path.join(__static, 'extensions/react-developer-tools/3.6.0_0')
   }
 ]
 
-export default function initDevTools(window) {
-  const installedExtensions = Object.keys(BrowserWindow.getDevToolsExtensions())
-  // console.debug('[main/initDevTools] installedExtensions', installedExtensions)
-  extensions.forEach(({ name, dir }) => {
-    if (!installedExtensions.includes(name)) {
-      console.debug('[main/initDevTools] addDevToolsExtension', { name, dir })
-      BrowserWindow.addDevToolsExtension(dir)
-    }
-  })
-
+export default function initDevTools(window, withExtensions) {
   window.webContents.on('devtools-opened', () => {
     window.focus()
     setImmediate(() => window.focus())
@@ -28,4 +20,24 @@ export default function initDevTools(window) {
   window.webContents.on('did-frame-finish-load', () => {
     window.webContents.openDevTools()
   })
+  if (withExtensions) {
+    installExtensions()
+  }
+}
+
+export function installExtensions(extensions = customExtensions) {
+  const installedExtensions = Object.keys(BrowserWindow.getDevToolsExtensions())
+  extensions.forEach(({ name, dir }) => {
+    if (!installedExtensions.includes(name)) {
+      BrowserWindow.addDevToolsExtension(dir)
+    }
+  })
+}
+
+export function uninstallExtensions() {
+  const installedExtensions = Object.keys(BrowserWindow.getDevToolsExtensions())
+  // console.debug('[main/initDevTools] uninstallExtensions', { installedExtensions })
+  installedExtensions.forEach(name =>
+    BrowserWindow.removeDevToolsExtension(name)
+  )
 }
