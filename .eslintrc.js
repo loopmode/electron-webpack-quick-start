5d2598d9036615a8550901a68cf04af4d17bc58c
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-04-12 08:27:16 +0200

    allow unused rest siblings

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     // allow console and debugger in development
     'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars': [1, { ignoreRestSiblings: true }]
   }
 }
