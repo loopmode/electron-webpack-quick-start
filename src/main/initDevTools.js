6a72042dbcf60ee7d680f271c46802491c17cb49
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-04-12 10:33:17 +0200

    install only missing extensions

--- src/main/initDevTools.js
+++ src/main/initDevTools.js
@@ -1,21 +1,31 @@
 import { BrowserWindow } from 'electron'
 import * as path from 'path'
 
+// custom extensions to install
+// use BrowserWindow.getDevToolsExtensions() to get name
+
 const extensions = [
-  path.join(__static, 'extensions/immutablejs-object-formatter/1.9.2_0')
+  {
+    name: 'Immutable.js Object Formatter',
+    dir: path.join(__static, 'extensions/immutablejs-object-formatter/1.9.2_0')
+  }
 ]
 
 export default function initDevTools(window) {
+  const installedExtensions = Object.keys(BrowserWindow.getDevToolsExtensions())
+  // console.debug('[main/initDevTools] installedExtensions', installedExtensions)
+  extensions.forEach(({ name, dir }) => {
+    if (!installedExtensions.includes(name)) {
+      console.debug('[main/initDevTools] addDevToolsExtension', { name, dir })
+      BrowserWindow.addDevToolsExtension(dir)
+    }
+  })
+
   window.webContents.on('devtools-opened', () => {
     window.focus()
-    setImmediate(() => {
-      window.focus()
-    })
+    setImmediate(() => window.focus())
   })
   window.webContents.on('did-frame-finish-load', () => {
-    extensions.forEach(extensionPath => {
-      BrowserWindow.addDevToolsExtension(extensionPath)
-    })
     window.webContents.openDevTools()
   })
 }
