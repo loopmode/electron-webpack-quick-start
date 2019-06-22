f669e594ebc586fbdf0ca114e3c3a46359d46f8a
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-06-22 10:51:33 +0200

    use webpack config as function

--- webpack.renderer.additions.js
+++ webpack.renderer.additions.js
@@ -1,11 +1,17 @@
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include: /node_modules/,
-        use: ['react-hot-loader/webpack']
-      }
-    ]
-  }
+const merge = require('webpack-merge')
+
+module.exports = function(config) {
+  config = merge.smart(config, {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: /node_modules/,
+          use: ['react-hot-loader/webpack']
+        }
+      ]
+    }
+  })
+
+  return config
 }
