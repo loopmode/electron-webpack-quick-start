38416c288d86713457111a5d2494aba8c5224dbb
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-06-22 21:29:03 +0200

    init

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -2,12 +2,26 @@ import { hot } from 'react-hot-loader/root'
 import React from 'react'
 
 import { getStatic } from '@/static'
+import autobind from 'autobind-decorator'
+class DecoratorDummy extends React.Component {
+  message = 'if you see this in the console log, then it works!'
+  render() {
+    return (
+      <button onClick={this.handleClick}>click me and watch the console log</button>
+    )
+  }
+  @autobind
+  handleClick() {
+    console.log(this.message)
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
       <section>
+        <DecoratorDummy />
         <h2>Embedded</h2>
         <p>
           The next image is embedded via{' '}
